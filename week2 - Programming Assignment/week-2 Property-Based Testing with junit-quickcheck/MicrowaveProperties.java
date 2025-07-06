642a639cc3f2294c8448a0ae88e3829e2465a03d
Samkeleni Maduna <samkelenim@gmail.com>
2025-07-07 00:12:09 +0200

    Update MicrowaveProperties.java

--- week2 - Programming Assignment/week-2 Property-Based Testing with junit-quickcheck/MicrowaveProperties.java
+++ week2 - Programming Assignment/week-2 Property-Based Testing with junit-quickcheck/MicrowaveProperties.java
@@ -15,27 +15,6 @@ public class MicrowaveProperties {
 		timedCondition = new TrueWithin(microwave.getTickRateInHz() + 1);
 	}
 	
-	/**
-	 * checkProperties: this method checks whether the microwave is meeting its 
-	 * behavioral requirements.  It requires the current and previous values of 
-	 * the microwave inputs (InputRecord) and state (OutputRecord).
-	 * 
-	 * Your job for this assignment is to take the requirements and create Java
-	 * code to check whether they are true.  
-	 * 
-	 * It is sometimes tricky to determine whether a requirement is talking 
-	 * about the 'current state' or the 'previous state'.  For example, often
-	 * a requirement says something like: when the system is in mode X, and Y happens,
-	 * then the system shall enter mode Z.  In this case, mode X is the previous 
-	 * system mode and mode Z is the current system mode.  Usually Y is a current 
-	 * state input, but read carefully!
-	 * 
-	 * @param previousInput
-	 * @param previousOutput
-	 * @param currentInput
-	 * @param currentOutput
-	 * 
-	 */
 	public void checkProperties(InputRecord previousInput, OutputRecord previousOutput,
 			   InputRecord currentInput, OutputRecord currentOutput) {
 				
@@ -51,46 +30,77 @@ public class MicrowaveProperties {
 				   implies(currentInput.clearPressed && previousOutput.mode == ModeController.Mode.Cooking, 
 						   currentOutput.mode != ModeController.Mode.Cooking));
 
-		// replace 'true' with the property definition.
+		// If current time to cook is zero, then mode is setup mode.
 		assertTrue("If current time to cook is zero, then mode is setup mode.",
-				true);
+				implies(currentOutput.secondsToCook == 0, 
+						currentOutput.mode == ModeController.Mode.Setup));
 		
+		// When the microwave is cooking, the time is either the same or decreases by one
 		assertTrue("When the microwave is cooking, the time is either the same or " + 
 				   "decreases by one",
-				true);
+				implies(previousOutput.mode == ModeController.Mode.Cooking && 
+						currentOutput.mode == ModeController.Mode.Cooking,
+						(currentOutput.secondsToCook == previousOutput.secondsToCook || 
+						 currentOutput.secondsToCook == previousOutput.secondsToCook - 1)));
 		
+		// seconds to cook is always >= 0
 		assertTrue("seconds to cook is always >= 0", 
-					true);
+					currentOutput.secondsToCook >= 0);
 
+		// At the instant when the clear button is pressed, if the microwave is in suspended mode, 
+		// it shall enter the setup mode
 		assertTrue("At the instant when the clear button is pressed, if the " + 
 				  "microwave is in suspended mode, it shall enter the setup mode", 
-				  true);
+				  implies(currentInput.clearPressed && previousOutput.mode == ModeController.Mode.Suspended,
+						  currentOutput.mode == ModeController.Mode.Setup));
 		
+		// If in suspended mode, at the instant the start key is pressed the microwave shall 
+		// enter cooking mode if the door is closed and the seconds_to_cook is greater than 0
 		assertTrue("If in suspended mode, at the instant the start key is pressed " + 
 				   "the microwave shall enter cooking mode if the door is closed and " + 
 				   "the seconds_to_cook is greater than 0", 
-				   true);
+				   implies(previousOutput.mode == ModeController.Mode.Suspended && 
+						   currentInput.startPressed && 
+						   !currentInput.doorOpen && 
+						   previousOutput.secondsToCook > 0,
+						   currentOutput.mode == ModeController.Mode.Cooking));
 
+		// If in suspended mode, at the instant the start key is pressed the microwave shall 
+		// remain in suspended mode if the door is open and the seconds_to_cook is greater than 0
 		assertTrue("If in suspended mode, at the instant the start key is pressed " + 
 				   "the microwave shall remain in suspended mode if the door is open and " + 
 				   "the seconds_to_cook is greater than 0", 
-				   true);
+				   implies(previousOutput.mode == ModeController.Mode.Suspended && 
+						   currentInput.startPressed && 
+						   currentInput.doorOpen && 
+						   previousOutput.secondsToCook > 0,
+						   currentOutput.mode == ModeController.Mode.Suspended));
 
+		// If the mode is setup or suspended and the clear button is pressed, the steps to cook shall be zero
 		assertTrue("If the mode is setup or suspended and the clear button is pressed, " +
 				   "the steps to cook shall be zero", 
-					true);	
-
-
+					implies((previousOutput.mode == ModeController.Mode.Setup || 
+							previousOutput.mode == ModeController.Mode.Suspended) && 
+							currentInput.clearPressed,
+							currentOutput.secondsToCook == 0));	
+		
+		// If the display is quiescent (no buttons pressed) and mode was setup, the mode is still setup 
+		// and the seconds_to_cook shall not change.
 		assertTrue("If the display is quiescent (no buttons pressed) and mode was setup, " + 
 					"the mode is still setup and the seconds_to_cook shall not change.", 
-					true);
+					implies(previousOutput.mode == ModeController.Mode.Setup && 
+							!currentInput.startPressed && 
+							!currentInput.clearPressed && 
+							!currentInput.digitPressed,
+							currentOutput.mode == ModeController.Mode.Setup && 
+							currentOutput.secondsToCook == previousOutput.secondsToCook));
 		
-		// Note: this example is tricky: we want to check that within a certain number
-		// of "checks" that the condition is true.  We use the TrueWithin class to check this.
-		// Still, replace 'true' with the property definition.
+		// When the microwave is cooking, within tickRateInHz ticks, the time to cook will decrease
 		assertTrue("When the microwave is cooking, within tickRateInHz ticks, the time to cook will decrease",
-				timedCondition.check(true));
-
+				timedCondition.check(
+					previousOutput.mode == ModeController.Mode.Cooking && 
+					currentOutput.mode == ModeController.Mode.Cooking && 
+					currentOutput.secondsToCook < previousOutput.secondsToCook));
 
 		} catch (Throwable e) {
 			System.out.println("Exception occurred during property check.  ");
@@ -101,6 +111,5 @@ public class MicrowaveProperties {
 			System.out.println("Current Output: " + currentOutput.toString());
 			throw e;
 		}
-		
 	} 
 }
